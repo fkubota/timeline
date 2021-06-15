b7722c7af519bf435cbd2bf88317ef3f1769051b
fkubota <rkkubotay@gmail.com>
2021-06-15 14:00:17 +0900

    [update] build

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,16 @@
 //   ]
 // }
 module.exports = {
-  "outputDir": "docs",
-  "assetsDir": "./",
-  "publicPath": "./",
-  "transpileDependencies": [
-    "vuetify"
-  ],
-    pages: {
-    index: {
-      entry: 'src/main.js', // 必須パラメータ
-      title: "fkubota's timeline",
+    "outputDir": "docs",
+    "assetsDir": "./",
+    "publicPath": "./",
+    "transpileDependencies": [
+        "vuetify"
+    ],
+
+    pages: { index: {
+        entry: 'src/main.js', // 必須パラメータ
+        title: "fkubota's timeline",
     }
-  }
+    },
 }
