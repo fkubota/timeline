e5bb91063b5825960722c3e0deadfc8d66a9b92e
fkubota <rkkubotay@gmail.com>
2021-06-20 16:36:19 +0900

    [update] build

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,23 @@
 import Timeline from './components/Timeline';
 
 export default {
-  name: 'App',
-  components: {
-    Timeline,
-  },
-  data: () => ({
-    //
-  }),
+        name: 'App',
+        components: {
+                Timeline,
+            },
+        data: () => ({
+                //
+            }),
+        metaInfo() {
+                return {
+                        meta: [
+                                // Twitter Card
+                                {name: 'twitter:site', content: '@fkubota_'},
+                                {name: 'twitter:card', content: 'summary'},
+                                {name: 'twitter:title', content: 'Vue Social Cards Example'},
+                                {name: 'twitter:description', content: 'Vue sample site showing off Twitter and Facebook Cards.'},
+                            ]
+                    }
+            }
 };
 </script>
