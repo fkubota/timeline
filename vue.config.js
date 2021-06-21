0e73160b67dc3aa0e61ed4d31c96aca1a53523b6
fkubota <rkkubotay@gmail.com>
2021-06-21 22:57:28 +0900

    [update]

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   pages: {
     index: {
       entry: 'src/main.js', // 必須パラメータ
-      title: "fkubota's timeline",
+      title: "timeline",
       template: 'public/index.html'
     }
   },
