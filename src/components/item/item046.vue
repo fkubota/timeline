8476ed68cbd55afcf4a52b80d79c449a7f713f74
fkubota <rkkubotay@gmail.com>
2021-06-22 14:15:48 +0900

    [update] 記事の追加。bookshelf

---
+++ src/components/item/item046.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-hover>
+        <v-timeline-item icon="mdi-github" color='#208ea3'
+                     slot-scope="{ hover  }"
+                     :class="`${hover? 'class1': 'class2'}`">
+            <v-row class="pt-1">
+                <v-col cols="3">
+                    <strong class='contents-title'>2021/06/21</strong>
+                </v-col>
+                <v-col>
+                    <h2 class='contents-title'>「bookshelf」を公開</h2>
+                    <div class="text-caption">
+                        <div class='contents-text'>
+                            読んだ論文や書籍をまとめるためのwebサイト。<br>
+                            主に自分用。
+
+                        </div>
+                    </div>
+                    <v-btn
+                            text
+                            style="color: #208ea3"
+                            @click="reveal = true"
+                            href='https://fkubota.github.io/bookshelf/'
+                            >
+                            Learn More
+                    </v-btn>
+                </v-col>
+                <v-col>
+                    <v-img src="./../../images/bg014.png" max-width='300' max-height='200' contain> </v-img>
+                </v-col>
+                <v-col>
+                    <Tweet :options="{width: 200}" id='1341038462128930816'></Tweet>
+                </v-col>
+            </v-row>
+        </v-timeline-item>
+    </v-hover>
+</template>
+
+<script>
+    import { Tweet  } from 'vue-tweet-embed'
+    export default {
+            components: {
+                    Tweet,
+                }
+        }
+</script>
+
+<style>
+  @import "./../../css/mycss.css";
+</style>
