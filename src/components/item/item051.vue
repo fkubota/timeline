7d330cb988323c8566025578b3c59d3acb756be5
fkubota <rkkubotay@gmail.com>
2021-07-14 14:17:49 +0900

    [add] item051, hbd 28 years old

---
+++ src/components/item/item051.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-hover>
+    <v-timeline-item color="#e4e2e2ff" small 
+                     slot-scope="{ hover  }"
+                     :class="`${hover? 'class1': 'class2'}`">
+            <v-row class="pt-1">
+                <v-col cols="3">
+                  <strong class='contents-title'>{{ date_ }}</strong>
+                </v-col>
+                <v-col>
+                  <strong class='contents-title'>{{ title }}</strong>
+                    <div class="text-caption">
+                      {{ body }}
+                    </div>
+                </v-col>
+                <v-col>
+                  <Tweet :options="{width: 200}" :id='tweet_id'></Tweet>
+                </v-col>
+            </v-row>
+    </v-timeline-item>
+    </v-hover>
+</template>
+
+<script>
+  import { Tweet  } from 'vue-tweet-embed'
+  export default {
+      components: {
+          Tweet
+        },
+    data () {
+        return {
+            date_: '2021/07/13',
+            title: '28歳になりました',
+            body: '28歳は仕事に打ち込みたいです。(残業せず、心身ともに健康を維持しつつ)',
+            tweet_id: '1414727648043945986'
+          }
+      }
+    }
+
+</script>
+
+<style>
+@import "./../../css/mycss.css";
+</style>
+
