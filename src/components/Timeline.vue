86fcb5169c93a705681b41d1d4e941bf240fb398
fkubota <rkkubotay@gmail.com>
2021-08-21 15:39:57 +0900

    [update] portfolio linkの追加

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -12,6 +12,14 @@
       <v-spacer></v-spacer>
       <v-tooltip bottom>
           <template v-slot:activator="{ on, attrs }">
+              <v-btn icon href='https://fkubota.github.io/portfolio/' v-bind="attrs" v-on="on">
+                  <v-icon>mdi-account</v-icon>
+              </v-btn>
+          </template>
+          <span>portfolio</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
               <v-btn icon href='https://fkubota.github.io/bookshelf/' v-bind="attrs" v-on="on">
                   <v-icon>mdi-bookshelf</v-icon>
               </v-btn>
